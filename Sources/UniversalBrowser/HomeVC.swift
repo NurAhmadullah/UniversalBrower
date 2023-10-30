35534351be0c1b01ad1cf21bd1af75cd5c292eb0
nur.ahmadullah <nur.ahmadullah@bjitgroup.com>
2023-10-30 11:52:20 +0600

    first commit

---
+++ Sources/UniversalBrowser/HomeVC.swift
@@ -0,0 +1,18 @@
+//
+//  HomeVC.swift
+//  
+//
+//  Created by BJIT on 30/10/23.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+}
--- Sources/UniversalBrowser/UniversalBrowser.swift
+++ Sources/UniversalBrowser/UniversalBrowser.swift
@@ -1,6 +1,10 @@
+import UIKit
 public struct UniversalBrowser {
     public private(set) var text = "Hello, World!"
 
     public init() {
+//        if let vc = UIStoryboard(name: "UBBoard", bundle: nil).instantiateViewController(withIdentifier: "HomeVC") as? HomeVC {
+//            self.present(vc)
+//        }
     }
 }
